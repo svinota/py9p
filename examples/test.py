32d9d499de7e2fddd62753d8de671a65111e7002
Peter V. Saveliev <peet@redhat.com>
2013-01-06 17:41:08 +0100

    test: bring it back

--- examples/test.py
+++ examples/test.py
@@ -3,7 +3,15 @@ import socket
 import sys
 import os
 import timeit
-from py9p import py9p
+import py9p
+
+try:
+    assert py9p.version > "1.0.6"
+    from py9p import py9p
+    assert hasattr(py9p, "Credentials")
+    ng = True
+except:
+    ng = False
 
 class CmdClient(py9p.Client):
 
@@ -24,6 +32,10 @@ if __name__ == "__main__":
         print "%s" % ( e.args[1])
         sys.exit(255)
 
-    cl = CmdClient(py9p.Sock(sock, 0, 0), 'none', user, None, None, 0, key=None)
+    if ng:
+        cl = CmdClient(sock, py9p.Credentials(user), None)
+    else:
+        cl = CmdClient(py9p.Sock(sock, 0, 0), 'none', user, None, None, 0)
+
     t = timeit.Timer('cl.cat("sample1")','from __main__ import cl')
     print "1000 cats (walk/open/read/clunk) in", t.timeit(1000), "seconds"
